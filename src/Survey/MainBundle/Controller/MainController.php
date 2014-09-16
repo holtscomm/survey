ca95559e787fc80a3c1ec8e4766dae945b55b3a3
Graham Holtslander <menello@gmail.com>
2014-09-15 22:14:56 -0600

    Fixes #5 - Adding Questions to the project
    @gholtslander
    
    I've added code that will handle creating and using the questions for the survey. Unit tests included! Just waiting for the last half of the mappings. I wonder if I can look it up online or something...

--- src/Survey/MainBundle/Controller/MainController.php
+++ src/Survey/MainBundle/Controller/MainController.php
@@ -1,7 +1,9 @@
 <?php
 namespace Survey\MainBundle\Controller;
 
+use Survey\MainBundle\Entity\Question;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class MainController extends Controller
 {
@@ -9,6 +11,51 @@ class MainController extends Controller
     {
         return $this->render(
             'SurveyMainBundle:Main:index.html.twig'
+        );
+    }
+
+    public function createAction()
+    {
+        $question = new Question();
+        $question->setText("This question has text! Huzzah!");
+        $question->setCategory("healing");
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($question);
+        $em->flush();
+
+        return new Response('Created new question with id '. $question->getId());
+    }
+
+    public function showAction($id)
+    {
+        $question = $this->getDoctrine()
+            ->getRepository('SurveyMainBundle:Question')
+            ->find($id);
+
+        if(!$question)
+        {
+            throw $this->createNotFoundException("No question found for id ".$id);
+        }
+
+        return $this->render(
+            'SurveyMainBundle:Main:index.html.twig', 
+            array("question"=>$question)
+        );
+    }
+
+    public function showByCategoryAction($category)
+    {
+        $repository = $this->getDoctrine()
+            ->getRepository('SurveyMainBundle:Question');
+
+        $questions = $repository->findBy(
+            array('category' => $category)
+        );
+
+        return $this->render(
+            'SurveyMainBundle:Main:index.html.twig', 
+            array("questions" => $questions)
         );
     }
 }
