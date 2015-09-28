310ed32357d384e192bc381f4674b8dc84f6b5c1
Graham Holtslander <graham@holtslander.com>
2015-09-28 06:53:29 -0600

    Create JsonApiHandler class for apis

--- src/app/views/api/__init__.py
+++ src/app/views/api/__init__.py
@@ -1,3 +1,15 @@
 """
 API endpoints
 """
+import json
+import webapp2
+
+
+class JsonApiHandler(webapp2.RequestHandler):
+    def return_json_response(self, data, status_code=200):
+        return_dict = {
+            'data': data,
+            'status': status_code,
+        }
+
+        self.response.out.write(json.dumps(return_dict))
