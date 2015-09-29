b0a38d8cb55f896ad37fa31cb67c7f84bffd262d
Graham Holtslander <graham@holtslander.com>
2015-09-29 07:09:34 -0600

    React the es6+ way

--- src/static/js/app/survey.js
+++ src/static/js/app/survey.js
@@ -1,13 +1,47 @@
 import fetch from 'fetch';
 import React from 'react';
 
-class Survey extends React.Component {
-  getNewPage(number) {
+class SurveyQuestion extends React.Component {
+  render() {
+    console.log(this.props);
+    let questionClassName = 'question-number-' + this.props.questionNumber;
+    return (<div className={questionClassName}>
+        {this.props.questionNumber} {this.props.questionText} {this.props.questionAnswer} {this.props.questionCategory}
+      </div>);
+  }
+}
 
+class SurveyPage extends React.Component {
+  state = {
+    questions: this.props.questions.map((question) => {
+      return <SurveyQuestion key={question.question_number} questionText={question.text}
+                             questionNumber={question.question_number}
+                             questionAnswer={question.answer} questionCategory={question.category}/>;
+    })
   }
 
   render() {
-    return <h1>Hello World!</h1>
+    return (
+      <div className='survey-page'>
+        {this.state.questions}
+      </div>
+    )
+  }
+}
+
+class Survey extends React.Component {
+  render() {
+    let questions = [
+      {
+      category: "adm",
+      question_number: 21,
+      text: "I am able to discern when to delegate important responsibilities, and to whom.",
+      answer: 0
+      }
+    ];
+    return (<div className='survey'>
+        <SurveyPage questions={questions} />
+      </div>);
   }
 }
 
