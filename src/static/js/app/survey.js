3609f63f9a689b7fbf1d869d023d7fbf7fd23be0
Graham Holtslander <graham@holtslander.com>
2015-10-01 06:07:43 -0600

    Finish survey in static React, add some dynamic loading

--- src/static/js/app/survey.js
+++ src/static/js/app/survey.js
@@ -1,22 +1,68 @@
-import fetch from 'fetch';
+import 'fetch';
 import React from 'react';
+
+class SurveyAnswerGroup extends React.Component {
+  render() {
+    let answerName = "choice" + this.props.questionNumber;
+    let alwaysSelected = this.props.questionAnswer == 5 ? 'checked' : '';
+    return (
+      <span className="col-md-3 col-xs-12">
+        <span className="col-xs-4 question-choice">
+            <span>Always</span>
+            <input
+              type="radio"
+              name={answerName}
+              value="2"
+              required />
+        </span>
+        <span className="col-xs-4 question-choice">
+            <span>Sometimes</span>
+            <input
+              type="radio"
+              name={answerName}
+              value="1"
+              required />
+        </span>
+        <span className="col-xs-4 question-choice">
+            <span>Rarely</span>
+            <input
+              type="radio"
+              name={answerName}
+              value="0"
+              required />
+        </span>
+      </span>
+    );
+  }
+}
 
 class SurveyQuestion extends React.Component {
   render() {
-    console.log(this.props);
-    let questionClassName = 'question-number-' + this.props.questionNumber;
-    return (<div className={questionClassName}>
-        {this.props.questionNumber} {this.props.questionText} {this.props.questionAnswer} {this.props.questionCategory}
-      </div>);
+    let questionClassName = 'row question-item question-number-' + this.props.questionNumber;
+    return (
+      <div className={questionClassName}>
+        <span className="col-md-1 col-xs-1 question-number">{ this.props.questionNumber }.</span>
+        <span className="col-md-8 col-xs-11 question-text">{ this.props.questionText }</span>
+        <SurveyAnswerGroup
+          questionAnswer={this.props.questionAnswer}
+          questionNumber={this.props.questionNumber}
+          />
+      </div>
+    );
   }
 }
 
 class SurveyPage extends React.Component {
   render() {
     let questions = this.props.questions.map((question) => {
-      return <SurveyQuestion key={question.question_number} questionText={question.text}
-                             questionNumber={question.question_number}
-                             questionAnswer={question.answer} questionCategory={question.category}/>;
+      return (
+        <SurveyQuestion
+          key={question.question_number}
+          questionText={question.text}
+          questionNumber={question.question_number}
+          questionAnswer={question.answer}
+          questionCategory={question.category}/>
+      );
     });
     return (
       <div className='survey-page'>
@@ -27,17 +73,32 @@ class SurveyPage extends React.Component {
 }
 
 class Survey extends React.Component {
+  state = {
+    questions: []
+  }
+
+  componentDidMount() {
+    this.getSurveyPage(1, 1);
+  }
+
+  getSurveyPage(userId, pageNum) {
+    let saved = fetch('/api/v1/survey/1/1')
+      .then((response) => response.json())
+      .then((jsonData) => this.updateQuestionsInState(jsonData));
+    // return fetch('/api/v1/survey/1/1')
+    //   .then((response) => response.json());
+  }
+
+  updateQuestionsInState(questionJson) {
+    this.setState({
+      questions: questionJson.data
+    });
+  }
+
   render() {
-    let questions = [
-      {
-      category: "adm",
-      question_number: 21,
-      text: "I am able to discern when to delegate important responsibilities, and to whom.",
-      answer: 0
-      }
-    ];
     return (<div className='survey'>
-        <SurveyPage questions={questions} />
+        <SurveyPage questions={this.state.questions} />
+        
       </div>);
   }
 }
