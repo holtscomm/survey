c8e61d5bff069673ffabd4f6b70e47ea9358989c
Graham Holtslander <menello@gmail.com>
2015-11-21 14:49:07 -0600

    Re-order answers low to high
    This is better for getting a true gauge of people

--- src/static/js/app/components/SurveyAnswerGroup.js
+++ src/static/js/app/components/SurveyAnswerGroup.js
@@ -5,7 +5,7 @@ export default class SurveyAnswerGroup extends React.Component {
     questionNumber: React.PropTypes.number.isRequired,
     questionAnswer: React.PropTypes.number,
     hasErrors: React.PropTypes.bool
-  }
+  };
 
   componentDidMount() {
     if (this.props.questionAnswer == 5) {
@@ -20,7 +20,7 @@ export default class SurveyAnswerGroup extends React.Component {
   handleOnChange = (e) => {
     this.refs.choices.classList.remove('has-error');
     this.props.handleOnChangeCallback(e);
-  }
+  };
 
   render() {
     let answerName = 'choice' + this.props.questionNumber;
@@ -30,43 +30,43 @@ export default class SurveyAnswerGroup extends React.Component {
         ref='choices'
         className={'survey-page__question-choices' + errorClass}>
         <span className='survey-page__question-choice'>
-            <label htmlFor={answerName + 'always'}>
-              <input
-                type='radio'
-                id={answerName + 'always'}
-                name={answerName}
-                ref='always'
-                value='5'
-                onChange={this.handleOnChange}
-                required />
-              <span>Usually</span>
-            </label>
+          <label htmlFor={answerName + 'rarely'}>
+            <input
+              type='radio'
+              id={answerName + 'rarely'}
+              name={answerName}
+              ref='rarely'
+              value='0'
+              onChange={this.handleOnChange}
+              required />
+            <span>Rarely</span>
+          </label>
         </span>
         <span className='survey-page__question-choice'>
-            <label htmlFor={answerName + 'sometimes'}>
-              <input
-                type='radio'
-                id={answerName + 'sometimes'}
-                name={answerName}
-                ref='sometimes'
-                value='2'
-                onChange={this.handleOnChange}
-                required />
-              <span>Sometimes</span>
-            </label>
+          <label htmlFor={answerName + 'sometimes'}>
+            <input
+              type='radio'
+              id={answerName + 'sometimes'}
+              name={answerName}
+              ref='sometimes'
+              value='2'
+              onChange={this.handleOnChange}
+              required />
+            <span>Sometimes</span>
+          </label>
         </span>
         <span className='survey-page__question-choice'>
-            <label htmlFor={answerName + 'rarely'}>
-              <input
-                type='radio'
-                id={answerName + 'rarely'}
-                name={answerName}
-                ref='rarely'
-                value='0'
-                onChange={this.handleOnChange}
-                required />
-              <span>Rarely</span>
-            </label>
+          <label htmlFor={answerName + 'always'}>
+            <input
+              type='radio'
+              id={answerName + 'always'}
+              name={answerName}
+              ref='always'
+              value='5'
+              onChange={this.handleOnChange}
+              required />
+            <span>Usually</span>
+          </label>
         </span>
       </span>
     );
