00ea0736c63dedc6f331226018181140f6265974
Graham Holtslander <menello@gmail.com>
2016-01-11 22:30:04 -0600

    Update frontend for new changes to server-side

--- src/static/js/app/components/Survey.js
+++ src/static/js/app/components/Survey.js
@@ -11,6 +11,9 @@ export default class Survey extends React.Component {
     questions: [],
     nextPage: 2
   };
+  static defaultProps = {
+    quizType: ''
+  };
 
   componentDidMount() {
     this.userId = document.getElementById('user-id').innerHTML;
@@ -24,11 +27,11 @@ export default class Survey extends React.Component {
   }
 
   getFirstPageForUser(userId) {
-    SurveyApi.getFirstPageForUserId(userId, this.updateQuestionsInState);
+    SurveyApi.getFirstPageForUserId(userId, this.props.quizType, this.updateQuestionsInState);
   }
 
   getNextPageOrSubmit = (e) => {
-    if (Object.keys(this._surveyPage.getQuestions()).length !== 20) {
+    if (Object.keys(this._surveyPage.getQuestions()).length !== 15) {
       this.setState({
         pageHasErrors: true
       });
@@ -47,11 +50,11 @@ export default class Survey extends React.Component {
   };
 
   getSurveyPage(userId, pageNum) {
-    SurveyApi.getSurveyPage(userId, pageNum, this.updateQuestionsInState);
+    SurveyApi.getSurveyPage(userId, pageNum, this.props.quizType, this.updateQuestionsInState);
   }
 
   submitAnswers(userId) {
-    SurveyApi.submitAnswers(userId, this._surveyPage.giveQuestionData());
+    SurveyApi.submitAnswers(userId, this._surveyPage.giveQuestionData(), this.props.quizType);
   }
 
   updateQuestionsInState = (questionJson) => {
