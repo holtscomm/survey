a93b316c1a01e43b8e912af1cff9214b3f0c9b4e
Graham Holtslander <gholtslander+github@vendasta.com>
2016-06-28 08:05:10 -0600

    Remove plugin so I will not forget to later

--- webpack.config.js
+++ webpack.config.js
@@ -24,14 +24,9 @@ module.exports = {
     },
     plugins: [
          new webpack.ProvidePlugin({
-            'Promise': 'es6-promise', // Thanks Aaron (https://gist.github.com/Couto/b29676dd1ab8714a818f#gistcomment-1584602)
+            'Promise': 'es6-promise',
             'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-        }),
-        new webpack.DefinePlugin({
-          'process.env':{
-            'NODE_ENV': JSON.stringify('production')
-          }
-        })
+	 })
     ],
     node: {
         net: "empty",
