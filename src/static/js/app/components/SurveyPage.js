0a3c27c1d08c8ff7b7a185f841123223b23578a5
Graham Holtslander <gholtslander+github@vendasta.com>
2016-06-27 22:22:08 -0600

    Give hrs keys so that react will not complain

--- src/static/js/app/components/SurveyPage.js
+++ src/static/js/app/components/SurveyPage.js
@@ -57,7 +57,7 @@ export default class SurveyPage extends React.Component {
           questionCategory={question.category}
           hasErrors={shouldHighlight}/>
       );
-      allQuestions.push(<hr />);
+      allQuestions.push(<hr key={question.question_number + 'hr'}/>);
     });
     if (answeredQuestions.length > 0) {
       this.setState({
