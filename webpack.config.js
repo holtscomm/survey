a1d7768d470ac78514066dc8959bcc398ec1d108
Graham Holtslander <gholtslander+github@vendasta.com>
2016-06-27 22:09:23 -0600

    Down with jspm, up with webpack!

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/static/js/app/survey.js',
+    output: {
+        filename: 'src/static/js/survey.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react'],
+                    plugins: ['transform-class-properties']
+                }
+            },
+            {
+                test: /\.json$/,
+                loader: 'json'
+            }
+        ]
+    },
+    plugins: [
+         new webpack.ProvidePlugin({
+            'Promise': 'es6-promise', // Thanks Aaron (https://gist.github.com/Couto/b29676dd1ab8714a818f#gistcomment-1584602)
+            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+        })
+    ],
+    node: {
+        net: "empty",
+        tls: "empty",
+        dns: "empty"
+    }
+};
