f131dfd4bf36d38cbaefa2381771db3fcdab81f6
Graham Holtslander <gholtslander+github@vendasta.com>
2016-07-01 11:11:50 -0600

    Actual working es6 promise polyfill now

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     plugins: [
          new webpack.ProvidePlugin({
-            'Promise': 'es6-promise',
+            'Promise': 'imports?this=>global!exports?global.Promise!es6-promise',
             'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
          })
     ],
