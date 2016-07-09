3dce0b09dc629da4041cf251ff02a2e5ee6b215d
Graham Holtslander <gholtslander+github@vendasta.com>
2016-07-09 08:38:57 -0600

    Fix routing, add more info to superadmin

--- src/app/views/superadmin.py
+++ src/app/views/superadmin.py
@@ -5,24 +5,27 @@ from app.models.quiz_attempt import QuizAttempt
 from app.models.user import User
 from app.views import TemplatedView
 
+
 class MainView(TemplatedView):
     @ndb.synctasklet
     def get_superadmin_index_data(self):
-        paid_survey_users, all_quiz_attempts = yield (
+        paid_survey_users, all_quiz_attempts, paid_quiz_attempts = yield (
             User.get_paid_survey_users().fetch_async(),
-            QuizAttempt.get_all_attempts().fetch_async()
+            QuizAttempt.get_all_attempts().fetch_async(),
+            QuizAttempt.get_non_trial_attempts().fetch_async()
         )
         all_users = yield ndb.get_multi_async([User.build_key(attempt.user_id) for attempt in all_quiz_attempts])
-        raise ndb.Return((paid_survey_users, all_quiz_attempts, all_users))
+        raise ndb.Return((paid_survey_users, all_quiz_attempts, all_users, paid_quiz_attempts))
 
     def get(self):
-        paid_survey_users, all_quiz_attempts, all_users = self.get_superadmin_index_data()
+        paid_survey_users, all_quiz_attempts, all_users, paid_quiz_attempts = self.get_superadmin_index_data()
 
         context = {
             'paid_survey_users': len(paid_survey_users),
             # 'paid_survey_users_last_30_days': len(User.get_paid_survey_users_last_30_days().fetch())
             'quiz_attempt_data': all_quiz_attempts,
-            'all_users': all_users
+            'all_users': all_users,
+            'paid_quiz_attempts': paid_quiz_attempts
         }
 
         self.render_response('superadmin/index.html', **context)
--- src/urls.py
+++ src/urls.py
@@ -2,18 +2,20 @@
 urls
 """
 
+from webapp2_extras.routes import RedirectRoute
+
 from webapp2 import Route
 
 ROUTES = [
-    Route('/superadmin/', handler='app.views.superadmin.MainView'),
-    Route('/superadmin/generate/', handler='app.views.superadmin.GenerateView'),
+    RedirectRoute('/superadmin/', name='superadmin-main', handler='app.views.superadmin.MainView'),
+    RedirectRoute('/superadmin/generate/', name='generate-surveys', handler='app.views.superadmin.GenerateView', strict_slash=True),
 
     Route('/', handler='app.views.main.MainView'),
-    Route('/gifts/', handler='app.views.survey.SurveyFullformView'),
-    Route('/gifts/a/', handler='app.views.survey.SurveyShortAView'),
-    Route('/gifts/b/', handler='app.views.survey.SurveyShortBView'),
-    Route('/gifts/trial/', handler='app.views.survey.SurveyTrialView'),
-    Route('/results/', handler='app.views.results.IndexView'),
+    RedirectRoute('/gifts/', name='full-gift-survey', handler='app.views.survey.SurveyFullformView', strict_slash=True),
+    RedirectRoute('/gifts/a/', name='short-a-gift-survey', handler='app.views.survey.SurveyShortAView', strict_slash=True),
+    RedirectRoute('/gifts/b/', name='short-b-gift-survey', handler='app.views.survey.SurveyShortBView', strict_slash=True),
+    RedirectRoute('/gifts/trial/', name='trial-gift-survey', handler='app.views.survey.SurveyTrialView', strict_slash=True),
+    RedirectRoute('/results/', name='results', handler='app.views.results.IndexView', strict_slash=True),
     Route('/api/v1/survey/<user_id:.+>/<page_num:\d+>/', handler='app.views.api.v1.survey.SurveyPageApiHandler'),
     Route('/api/v1/survey/getFirstPage/', handler='app.views.api.v1.survey.SurveyGetFirstPageApiHandler'),
     Route('/api/v1/survey/post/<user_id:.+>/', handler='app.views.api.v1.survey.SurveyPageApiHandler'),
