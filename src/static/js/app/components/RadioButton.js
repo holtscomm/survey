654ddd850fd40b460c76cba9a88123af99aa6c97
Graham Holtslander <gholtslander+github@vendasta.com>
2016-09-01 22:22:00 -0600

    Create and use RadioButton component

--- src/static/js/app/components/RadioButton.js
+++ src/static/js/app/components/RadioButton.js
@@ -1,3 +1,22 @@
-/**
- * Created by GHoltslander on 2016-09-01.
- */
+import React from 'react';
+
+export class RadioButton extends React.Component {
+  render() {
+    const {inputId, inputName, inputValue, handleOnChange, desktopText, mobileText} = this.props;
+    return (
+      <label htmlFor={inputId}>
+        <input
+          type='radio'
+          id={inputId}
+          name={inputName}
+          value={inputValue}
+          onChange={handleOnChange}
+          required/>
+        <span>
+          <span className="hidden-xs hidden-sm">{desktopText}</span>
+          <span className="hidden-md hidden-lg">{mobileText}</span>
+        </span>
+      </label>
+    );
+  };
+}
