054b2290ce4d13611ff4bd569a4c30ed58772da8
Graham Holtslander <menello@gmail.com>
2017-01-22 14:22:16 -0600

    Forgot to make one more thing secure!

--- src/settings.py
+++ src/settings.py
@@ -12,4 +12,4 @@ def is_devappserver():
     return True if os.environ['APPLICATION_ID'].startswith('dev') else False
 
 
-APP_URL = 'localhost:8079' if is_devappserver() else 'http://survey.holtscomm.ca'
+APP_URL = 'localhost:8079' if is_devappserver() else 'https://survey.holtscomm.ca'
